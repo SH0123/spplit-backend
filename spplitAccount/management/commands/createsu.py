2bd111849e334e49d6619790ce364b2312cf2370
Sanghyo <sanghyoboy@naver.com>
2021-08-13 14:32:28 +0900

    please

--- spplitAccount/management/commands/createsu.py
+++ spplitAccount/management/commands/createsu.py
@@ -1,10 +1,11 @@
 import os
 from django.core.management.base import BaseCommand
-from spplitAccount.models import User
+from spplitAccount.models import User, UserManager
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser('admin',
-                                          'shc3851@likelion.org',
+        if not User.objects.filter(email='shc3851@likelion.org').exists():
+            User.objects.create_superuser('shc3851@likelion.org', 
+                                          'admin',
+                                          '01012345678',
                                           'lifeisegg@00')
